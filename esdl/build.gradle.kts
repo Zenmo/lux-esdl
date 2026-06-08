d659c6a3d2e0d577ae7141e69602ab105937b0da
Erik van Velzen <erik@evanv.nl>
2026-06-08 18:10:39 +0200

    Move logback to test dependencies
    It doesn't belong in the main code as this is a library and the choice
    of logging framework is up to the application.
    
    And AnyLogic is shipped with log4j 2 and not Logback.

--- esdl/build.gradle.kts
+++ esdl/build.gradle.kts
@@ -28,11 +28,12 @@ dependencies {
     implementation("org.eclipse.emf:org.eclipse.emf.ecore.xmi:2.40.0")
 
     implementation("org.slf4j:slf4j-api:2.0.13")
-    implementation("ch.qos.logback:logback-classic:1.5.6")
+
 
     //testImplementation(platform("org.junit:junit-bom:6.0.3"))
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
     testImplementation("org.assertj:assertj-core:3.27.7")
+    testImplementation("ch.qos.logback:logback-classic:1.5.6")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
