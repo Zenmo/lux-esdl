218095b73e1a6e2da64eed3875c3f605b7f87066
Erik van Velzen <erik@evanv.nl>
2026-06-11 17:25:25 +0200

    Register profiles with the lux engine

--- core/src/main/java/energy/lux/esdl/core/loader/profile/UnitlessProfile.java
+++ core/src/main/java/energy/lux/esdl/core/loader/profile/UnitlessProfile.java
@@ -1,5 +1,9 @@
 package energy.lux.esdl.core.loader.profile;
 
+import zero_engine.EnergyModel;
+import zero_engine.J_ProfilePointer;
+import zero_engine.OL_ProfileUnits;
+
 /**
  * Data prepared to put into J_ProfilePointer
  */
@@ -8,4 +12,20 @@ public record UnitlessProfile(
         double step_h,
         double[] values
 ) {
+    /**
+     * Convert to final format and register with LUX Engine
+     */
+    public J_ProfilePointer toLuxProfile(EnergyModel luxModel, String name, OL_ProfileUnits unit) {
+        var luxProfile = new J_ProfilePointer(
+                name,
+                values,
+                step_h,
+                startRelativeToLuxStart_h,
+                OL_ProfileUnits.PRICE_EURPMWH
+        );
+
+        luxModel.f_addProfile(luxProfile);
+
+        return luxProfile;
+    }
 }
